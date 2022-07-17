79e78a3946b433d16a08f2894a174c41c0cf3d44
Artur <artur_goncharov_97@mail.ru>
2022-07-17 21:44:21 +0300

    connection with redis

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from typing import NoReturn
 from dotenv import load_dotenv
 
 import telegram
+import redis
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler
@@ -13,6 +14,8 @@ from telegram.ext import Filters
 def main():
     load_dotenv()
     tg_token = os.getenv("TG_TOKEN")
+    password_redis_db = os.getenv("REDIS_DB")
+    db_redis = redis.Redis(host='redis-12655.c299.asia-northeast1-1.gce.cloud.redislabs.com', port=12655, db=0, password=password_redis_db)
 
     updater = Updater(token=tg_token)
     dispacher = updater.dispatcher
@@ -22,7 +25,7 @@ def main():
     menu_keyboard = telegram.ReplyKeyboardMarkup(custom_keyboard)
     questions = get_quiz_question()
     dispacher.add_handler(CommandHandler("start", partial(start_callback, keyboard=menu_keyboard)))
-    dispacher.add_handler(MessageHandler(Filters.text, partial(responds_to_user, question=questions)))
+    dispacher.add_handler(MessageHandler(Filters.text, partial(responds_to_user, question=questions, db_redis=db_redis)))
 
     updater.start_polling()
 
@@ -31,12 +34,14 @@ def start_callback(update, context, keyboard) -> NoReturn:
     update.message.reply_text("Здравствуйте", reply_markup=keyboard)
 
 
-def responds_to_user(update, context, question) -> NoReturn:
+def responds_to_user(update, context, question, db_redis) -> NoReturn:
     message_text = update.message.text
+    chat_id = update.message.chat.id
     if message_text == "Новый вопрос":
         question_answer = next(question)
         new_question = question_answer[0]
-    update.message.reply_text(new_question)
+        db_redis.set(chat_id, new_question)
+    update.message.reply_text(db_redis.get(chat_id).decode())
 
 
 def get_quiz_question():
