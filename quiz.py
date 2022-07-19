b787148fb7f53a74c5dc668f1fd0567c2b03c969
Artur <artur_goncharov_97@mail.ru>
2022-07-19 22:57:27 +0300

    moved query generation to a separate module

---
+++ quiz.py
@@ -0,0 +1,20 @@
+import glob
+
+
+def get_quiz_question():
+    path_files_questions = glob.glob('quiz-questions/*.txt')
+    for path_file_questions in path_files_questions:
+        with open(path_file_questions, 'r', encoding="KOI8-R") as file:
+            file = file.read().split("\n\n")
+            questions_answers = {}
+            for item in file:
+                if "Вопрос" in item:
+                    question = item.split(":")
+                if "Ответ" in item:
+                    answer = item.split(":")
+                try:
+                    questions_answers[question[1]] = answer[1]
+                except:
+                    pass
+            for question, answer in questions_answers.items():
+                yield (question, answer)
