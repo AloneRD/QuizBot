963445122706dea80e33c3d628858ebc61ab848d
Artur <artur_goncharov_97@mail.ru>
2022-07-25 07:18:30 +0300

    loading questions in redis

--- quiz.py
+++ quiz.py
@@ -1,20 +1,36 @@
 import glob
+import redis
+import os
+import re
+import json
+from dotenv import load_dotenv
 
 
-def get_quiz_question():
+def upload_questions_redis(db_redis):
     path_files_questions = glob.glob('quiz-questions/*.txt')
+    question_number = 1
     for path_file_questions in path_files_questions:
         with open(path_file_questions, 'r', encoding="KOI8-R") as file:
             file = file.read().split("\n\n")
-            questions_answers = {}
             for item in file:
                 if "Вопрос" in item:
-                    question = item.split(":")
+                    question = re.sub(r"Вопрос \d*:", '', item)
                 if "Ответ" in item:
-                    answer = item.split(":")
-                try:
-                    questions_answers[question[1]] = answer[1]
-                except UnboundLocalError:
-                    pass
-            for question, answer in questions_answers.items():
-                yield (question, answer)
+                    answer = re.sub(r"Ответ:", '', item)
+                    question_answer = json.dumps({
+                        'question': question,
+                        'answer': answer
+                      })
+                    db_redis.set(f'question_{question_number}', question_answer)
+                    question_number += 1
+
+
+def main():
+    load_dotenv()
+    password_redis_db = os.getenv("REDIS_DB")
+    db_redis = redis.Redis(host='redis-12655.c299.asia-northeast1-1.gce.cloud.redislabs.com', port=12655, db=0, password=password_redis_db)
+    upload_questions_redis(db_redis)
+
+
+if __name__ =='__main__':
+    main()
